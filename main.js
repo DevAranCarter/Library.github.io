f1d11f70c8eb96ca8ee2bdf6f8995240838ea614
Devin Carter <devinacarter97@gmail.com>
2023-09-05 19:32:37 +0100

    Updated button styling

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ const titleval = document.getElementById('title');
 const authorval = document.getElementById('author')
 const yearPublishedVal = document.getElementById('yearPublished');
 const submit = document.getElementById('sbmt-btn');
+let read = document.getElementById('read-check');
+const bookCards = document.getElementById("book-cards");
+
+let bookCounter = 0;
+
 const myLibrary = [];
 
-function Book(title, author, yearPublished, about) {
+function Book(title, author, yearPublished) {
     this.title = title;
     this.author = author;
-    this.yearPublished = yearPublished;
-    this.about = about;
+    this.yearPublished = yearPublished; 
+
     
 }
 
@@ -20,8 +25,8 @@ function displayPopUp() {
 
 }
 
-function addBook(title, author, yearPublished){
-    let newBook = new Book(title, author, yearPublished);
+function addBook(title, author, yearPublished, read){
+    let newBook = new Book(title, author, yearPublished, read);
     myLibrary.push(newBook);
 
     console.log(myLibrary);
@@ -29,23 +34,74 @@ function addBook(title, author, yearPublished){
 }
 
 function bookDetails(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i].title);
-        console.log(array[i].author);
-        console.log(array[i].yearPublished);
-        console.log(array[i].about);
+    for (let i = bookCounter; i < array.length; i++) {
+        let newcard = document.createElement('div');
+        bookCards.appendChild(newcard);
+        newcard.style.backgroundColor = '#D3D3D3';
+        newcard.style.border = '2px solid black';
+        let title = document.createElement('h2');
+        title.innerText = array[i].title;
+        newcard.appendChild(title);
+        let author = document.createElement('h2');
+        author.innerText = array[i].author;
+        newcard.appendChild(author);
+        let yearPublished = document.createElement('h2');
+        yearPublished.innerText = array[i].yearPublished;
+        newcard.appendChild(yearPublished);
+        let hasread = document.createElement('button');
+        if (read.checked == false) {
+            hasread.style.backgroundColor = 'red';
+            hasread.textContent = 'Not read';
+            hasread.style.color = 'white';
+            hasread.style.borderRadius = '20px';
+        } 
+        else if (read.checked == true) {
+            hasread.style.backgroundColor = 'green';
+            hasread.textContent = 'Read';
+            hasread.style.color = 'white';
+            hasread.style.borderRadius = '20px';
+            hasread.style.border = 'none';
+        }
+        newcard.appendChild(hasread);
+        newcard.style.width = 'auto';
+        newcard.style.borderRadius = '12px';
+        newcard.style.textAlign = 'center';
+        newcard.style.padding = '20px';
+        hasread.style.boxShadow = '';
+        hasread.style.border = 'none';
+
+        bookCounter += 1;
 
     }
 }
 
+function checkRead() {
+    if (read === false) {
+        read.style.backgroundColor = red;
+    }
+    else {
+        read.style.backgroundColor = green;
+    }
+}
+
 function stopSubmit(event) {  
+    
+    event.preventDefault();
+    if (titleval.value && authorval.value && yearPublishedVal.value) {
     addBook(titleval.value, authorval.value, yearPublishedVal.value);
     bookDetails(myLibrary);
     popup.style.display = "none";
-    event.preventDefault();
     titleval.value = '';
-    authorval.value='';
-    yearPublishedVal.value = undefined;
+    authorval.value = '';
+    yearPublishedVal.value = '';
+    read.checked = false;
+    }
+    //else {
+    //titleval.attributes['required'] = true;
+   // authorval.attributes['required'] = true;
+    //yearPublishedVal.attributes['required'] = true;
+
+   // }
 
 }
 
@@ -53,7 +109,15 @@ popUpBtn.addEventListener('click', displayPopUp);
 
 close.addEventListener('click', ()=> {
     popup.style.display = "none";
+    titleval.value = '';
+    authorval.value = '';
+    yearPublishedVal.value = '';
+    read.checked = false;
 })
 
-submit.addEventListener('click', stopSubmit, false)
+submit.addEventListener('click', stopSubmit, false);
+
+console.log(read.value);
+
+
 
